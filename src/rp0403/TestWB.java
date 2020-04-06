3d58033c77a760517af0698d02cdb41327467dca
jason <jason@jason-PC>
2020-04-06 18:35:00 +0900

    0406

---
+++ src/rp0403/TestWB.java
@@ -0,0 +1,109 @@
+package rp0403;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JScrollPane;
+
+public class TestWB extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TestWB frame = new TestWB();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public TestWB() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 448, 594);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel, BorderLayout.NORTH);
+		
+		JLabel lblNewLabel = new JLabel("낚시가자");
+		panel.add(lblNewLabel);
+		
+		
+		JLabel lblImage = new JLabel();
+		setTitle("현재파일에서 그림넣기");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		contentPane.add(lblImage, BorderLayout.CENTER);
+			
+		setSize(403,678);
+		setVisible(true);
+		
+		lblImage.setIcon(new ImageIcon("d.jpg"));
+		contentPane.add(lblImage);
+		
+		JPanel panel_1 = new JPanel();
+		contentPane.add(panel_1, BorderLayout.SOUTH);
+		panel_1.setLayout(new BorderLayout(0, 0));
+		
+		JPanel panel_4 = new JPanel();
+		panel_1.add(panel_4, BorderLayout.NORTH);
+		
+		JLabel lblNewLabel_1 = new JLabel("아 이 디   :  ");
+		panel_4.add(lblNewLabel_1);
+		
+		textField = new JTextField();
+		panel_4.add(textField);
+		textField.setColumns(10);
+		
+		JPanel panel_5 = new JPanel();
+		panel_1.add(panel_5, BorderLayout.CENTER);
+		
+		JLabel lblNewLabel_2 = new JLabel("비밀번호 :  ");
+		panel_5.add(lblNewLabel_2);
+		
+		textField_1 = new JTextField();
+		panel_5.add(textField_1);
+		textField_1.setColumns(10);
+		
+		JPanel panel_6 = new JPanel();
+		panel_1.add(panel_6, BorderLayout.SOUTH);
+		
+		JButton btnNewButton = new JButton("회원가입");
+		panel_6.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("로 그 인");
+		panel_6.add(btnNewButton_1);
+		
+		JPanel panel_2 = new JPanel();
+		contentPane.add(panel_2, BorderLayout.WEST);
+		
+		JPanel panel_3 = new JPanel();
+		contentPane.add(panel_3, BorderLayout.EAST);
+		
+		
+		
+		
+	}
+}
